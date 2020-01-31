912e60fa95fe692d386395a50baf3a10239cfbd3
ace3a <ace3a@DESKTOP-57F0J0N>
2020-01-30 20:25:15 -0800

    Robotgraffiti

---
+++ src/_02_robot_graffiti/RobotGraffiti.java
@@ -0,0 +1,13 @@
+package _02_robot_graffiti;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+
+public class RobotGraffiti {
+public static void main(String[] args) throws AWTException {
+	Robot russell = new Robot();
+	
+
+
+}
+}
